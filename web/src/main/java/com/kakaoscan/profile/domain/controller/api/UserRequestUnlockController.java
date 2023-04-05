d48ac265b3739e80645216ad6093bfe400cd818a
ekfkawl <ekfkawl>
2023-04-06 02:41:09 +0900

    Chore: record log user role request message

--- web/src/main/java/com/kakaoscan/profile/domain/controller/api/UserRequestUnlockController.java
+++ web/src/main/java/com/kakaoscan/profile/domain/controller/api/UserRequestUnlockController.java
@@ -1,8 +1,12 @@
 package com.kakaoscan.profile.domain.controller.api;
 
 import com.kakaoscan.profile.domain.dto.UserDTO;
+import com.kakaoscan.profile.domain.dto.UserLogDTO;
 import com.kakaoscan.profile.domain.dto.UserRequestUnlockDTO;
 import com.kakaoscan.profile.domain.entity.UserRequestUnlock;
+import com.kakaoscan.profile.domain.enums.KafkaEventType;
+import com.kakaoscan.profile.domain.enums.LogType;
+import com.kakaoscan.profile.domain.kafka.service.KafkaProducerService;
 import com.kakaoscan.profile.domain.service.UserRequestUnlockService;
 import com.kakaoscan.profile.global.oauth.annotation.UserAttributes;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +16,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+
+import static com.kakaoscan.profile.utils.HttpRequestUtils.getRemoteAddress;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,12 +31,23 @@ public class UserRequestUnlockController extends ApiBaseController {
 
     private final UserRequestUnlockService userRequestUnlockService;
 
+    private final KafkaProducerService producerService;
+
     @PostMapping("/unlock")
     @PreAuthorize("hasRole('ROLE_GUEST')")
     public ResponseEntity<?> updateUnlockMessage(@Valid @RequestBody UserRequestUnlockDTO userRequestUnlockDTO,
-                                                 @UserAttributes UserDTO attributes) {
+                                                 @UserAttributes UserDTO attributes, HttpServletRequest request) {
 
         userRequestUnlockService.updateUnlockMessage(attributes.getEmail(), userRequestUnlockDTO.getMessage());
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("email", attributes.getEmail());
+        map.put("json", UserLogDTO.builder()
+                .type(LogType.UNLOCK.name())
+                .message(userRequestUnlockDTO.getMessage())
+                .remoteAddress(getRemoteAddress(request))
+                .build());
+        producerService.send(KafkaEventType.RECORD_LOG_EVENT, map);
 
         return ResponseEntity.ok().build();
     }
